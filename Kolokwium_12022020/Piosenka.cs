419c38a81ef7194349668a94947b0b0e43322f50
ixon7 <55660649+ixon7@users.noreply.github.com>
2020-02-12 17:53:01 +0100

    2

---
+++ Kolokwium_12022020/Piosenka.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kolokwium_12022020
+{
+    class Piosenka
+    {
+        public Piosenka(string tytul, string autor, TimeSpan dlugosc)
+        {
+            Tytul = tytul;
+            Autor = autor;
+            Dlugosc = dlugosc;
+        }
+
+        public string Tytul { get; set; }
+        public string Autor { get; set; }
+        public TimeSpan Dlugosc { get; set; }
+
+    }
+}
--- Kolokwium_12022020/Program.cs
+++ Kolokwium_12022020/Program.cs
@@ -10,7 +10,24 @@ namespace Kolokwium_12022020
     {
         static void Main(string[] args)
         {
-
+            List<Piosenka> Playlista = new List<Piosenka>();
+            TimeSpan date1 = new TimeSpan(2,3,4);
+            TimeSpan date2 = new TimeSpan(4, 5, 6);
+            TimeSpan date3 = new TimeSpan(7, 8, 4);
+            Playlista.Add(new Piosenka("Ala", "Brzechwa", date1));
+            Playlista.Add(new Piosenka("Ola", "Henryk", date2));
+            Playlista.Add(new Piosenka("Bolo", "Tuwim", date3));
+            Console.WriteLine("Twoja Playlista to:");
+            foreach (var i in Playlista)
+            {
+                Console.WriteLine($"{i.Autor} {i.Tytul} {i.Dlugosc}");
+            }
+            foreach(var i in Playlista)
+            {
+                Console.WriteLine($"Dlugosc playlisty {i.Dlugosc.Duration()}");
+            }
+            var alfatyt = Playlista.OrderBy(x => x.Tytul).ToList();
+            var alfaaut = Playlista.OrderBy(x => x.Autor).ToList();
         }
     }
 }
